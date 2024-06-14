807accc6cf8d25c75b23ce461dc58d8a5228630e
Karisha04 <karinapoplavskaya04@gmail.com>
2024-06-14 15:32:45 +0300

    Add reference to DAL
    Add database initialisation at api project

--- LX.StaffScheduler.Api/Program.cs
+++ LX.StaffScheduler.Api/Program.cs
@@ -1,3 +1,5 @@
+using LX.StaffScheduler.DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace LX.StaffScheduler.Api
 {
@@ -7,16 +9,16 @@ namespace LX.StaffScheduler.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
