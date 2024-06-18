d650b1cf1c2b30cd9761302637a91b1f49915be6
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2024-06-18 12:27:52 +0300

    Update according to review changes
     -Deleted AspNetCore changes
     -Comments deleted
     -Changed to defautl connection string

--- LX.StaffScheduler.Api/Controllers/CitiesController.cs
+++ LX.StaffScheduler.Api/Controllers/CitiesController.cs
@@ -1,40 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace LX.StaffScheduler.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CitiesController : ControllerBase
     {
-        // GET: api/<CitiesController>
+ 
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<CitiesController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<CitiesController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<CitiesController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<CitiesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
