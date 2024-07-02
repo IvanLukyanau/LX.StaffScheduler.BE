d0cefa265407b4ad5146382e8745ee3b9448c031
Nikita Rapkup <svinkapeppa75@GMAIL.COM>
2024-07-02 15:42:53 +0300

    Add save week Work shifs method

--- LX.StaffScheduler.DAL/Interfaces/IWorkShiftRepository.cs
+++ LX.StaffScheduler.DAL/Interfaces/IWorkShiftRepository.cs
@@ -5,5 +5,8 @@
         Task<IEnumerable<DateOnly>> GetMondaysWorkShiftsAsync(int cafeId);
         Task<IEnumerable<WorkShift>> GetWeekWorkShifts(int cafeId, DateOnly weekStartDate);
         Task<IEnumerable<WorkShift>> GetDayWorkShifts(int cafeId, DateOnly weekStartDate);
+
+        Task <IEnumerable<WorkShift>> SaveWeekWorkShifts(IEnumerable<WorkShift> workShifts);
+
     }
 }
